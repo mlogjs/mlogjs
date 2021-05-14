c283dd3cae89a7881b3c0d38b3db93e314daddf2
Wei <46848247+str1z@users.noreply.github.com>
2021-05-14 15:29:23 -0400

    removed build

--- build/handlers/Identifier.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Identifier = void 0;
-const Identifier = (c, scope, node) => {
-    return [scope.get(node.name), []];
-};
-exports.Identifier = Identifier;
-//# sourceMappingURL=Identifier.js.map
--- build/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nodeName = void 0;
-function nodeName(node) {
-    const { line, column } = node.loc.start;
-    return line + ":" + column;
-}
-exports.nodeName = nodeName;
-//# sourceMappingURL=utils.js.map
