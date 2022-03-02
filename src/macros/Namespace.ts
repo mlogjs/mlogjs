e1ddf9fe293d26cd9d3445ba80788c8a413dca77
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-03-01 23:15:27 -0300

    move casing transform function to utils file

--- src/macros/Namespace.ts
+++ src/macros/Namespace.ts
@@ -1,10 +1,7 @@
+import { camelToDashCase } from "../utils";
 import { MacroFunction } from ".";
 import { IScope } from "../types";
 import { LiteralValue, ObjectValue, StoreValue } from "../values";
-
-function transformCasing(name: string) {
-  return name.replace(/[A-Z]/g, str => `-${str.toLowerCase()}`);
-}
 
 interface NamespaceMacroOptions {
   changeCasing?: boolean;
@@ -20,7 +17,7 @@ export class NamespaceMacro extends ObjectValue {
         if (!(prop instanceof LiteralValue) || typeof prop.data !== "string")
           throw new Error("Cannot use dynamic properties on object macros");
         const symbolName = this.changeCasing
-          ? transformCasing(prop.data)
+          ? camelToDashCase(prop.data)
           : prop.data;
         return [new StoreValue(scope, `@${symbolName}`), []];
       }),
