f5a507badcb76c43f2dae15abb65817fd4950708
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-03-02 10:50:39 -0300

    fix GetLink return value

--- src/macros/commands/GetLink.ts
+++ src/macros/commands/GetLink.ts
@@ -2,6 +2,7 @@ import { InstructionBase } from "../../instructions";
 import { MacroFunction } from "..";
 import { IScope, IValue } from "../../types";
 import { LiteralValue, StoreValue, TempValue } from "../../values";
+import { Building } from "../Building";
 
 export class GetLink extends MacroFunction {
   constructor(scope: IScope) {
@@ -14,7 +15,8 @@ export class GetLink extends MacroFunction {
           "The getlink index must be a number literal or a store"
         );
       const temp = new TempValue(scope);
-      return [temp, [new InstructionBase("getlink", temp, index)]];
+      const outBuild = new Building(scope, temp.name);
+      return [outBuild, [new InstructionBase("getlink", temp, index)]];
     });
   }
 }
