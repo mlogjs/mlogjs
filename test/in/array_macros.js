fd3051ed6ee6cd64b832662a782102f21aaf012f
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2022-03-04 18:57:57 -0300

    add tests for array macros

---
+++ test/in/array_macros.js
@@ -0,0 +1,6 @@
+const array = [5, 19, 123, "foo"];
+
+print(array[0]);
+print(array[1]);
+print(array[2]);
+print(array[3]);
---
+++ test/in/array_macros_des.js
@@ -0,0 +1,9 @@
+// tests destructuring with array macros
+const array = [5, 19, 123, "foo"];
+
+const [a, b, c, d] = array;
+
+print(a);
+print(b);
+print(c);
+print(d);
