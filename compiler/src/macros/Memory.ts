3ead9d8646de227c88f371f6e8c82236ab66b5d3
JeanJPNM <jeanjpnm1@gmail.com>
2022-03-10 17:34:14 -0300

    fix error in memory assigment macro function

--- compiler/src/macros/Memory.ts
+++ compiler/src/macros/Memory.ts
@@ -19,7 +19,7 @@ class MemoryEntry extends ObjectValue {
         return [temp, [new InstructionBase("read", temp, mem.cell, prop)]];
       }),
       "$=": new MacroFunction(scope, value => {
-        return [null, [new InstructionBase("write", prop, mem.cell, value)]];
+        return [value, [new InstructionBase("write", prop, mem.cell, value)]];
       }),
     });
   }
