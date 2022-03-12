cf17455313e7f543c82e14f71b900a6441858dd5
JeanJPNM <jeanjpnm1@gmail.com>
2022-03-12 11:02:16 -0300

    add test for object macros

---
+++ compiler/test/in/object_macro.js
@@ -0,0 +1,15 @@
+const obj = {
+  0: "number index",
+  a: 10,
+  b: "String literal",
+  c(number) {
+    print("Number is ", number);
+  },
+};
+
+print(obj.a);
+print(obj.b);
+print(obj[0]);
+obj.c(20);
+
+printFlush(getBuilding("message1"));
