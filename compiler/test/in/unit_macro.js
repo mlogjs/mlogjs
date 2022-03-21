46d2b1beb2de1825d049a763e0f5507b1aaf6443
JeanJPNM <jeanjpnm1@gmail.com>
2022-03-21 19:50:03 -0300

    update test for unit macros

--- compiler/test/in/unit_macro.js
+++ compiler/test/in/unit_macro.js
@@ -10,3 +10,14 @@ print("health: ", Vars.unit.health, "\n");
 let item = Items.copper;
 print("amount of ", item, " : ", Vars.unit[item]);
 printFlush(getBuilding("message1"));
+
+const unitA = radar(
+  getBuilding("cyclone1"),
+  "enemy",
+  "any",
+  "any",
+  1,
+  "distance"
+);
+const unitB = unitRadar("enemy", "flying", "any", 1, "distance");
+print(unitA.health, unitB.health);
