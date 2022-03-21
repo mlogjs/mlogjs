72b254779e85a444c566b57b45c66ddb70463b77
JeanJPNM <jeanjpnm1@gmail.com>
2022-03-21 18:28:38 -0300

    update tests

--- compiler/test/fail.test.ts
+++ compiler/test/fail.test.ts
@@ -1,15 +1,18 @@
 import { readdirSync } from "fs";
 import { readFile } from "fs/promises";
 import { parse } from "path";
-import { compile } from "../src";
+import { Compiler } from "../src";
 
 const paths = readdirSync("test/fail");
 
+const compiler = new Compiler();
+
 for (const path of paths) {
   const { name } = parse(path);
+
   test(name + " will fail", async () => {
     const input = await readFile(`test/fail/${path}`, "utf8");
     const error = input.split("\n")[0].split("//")[1].trim();
-    expect(compile(input)[1]!.message).toBe(error);
+    expect(compiler.compile(input)[1]!.message).toBe(error);
   });
 }
