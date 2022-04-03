2dd44dfce6568a032b2561ccc2db361a2e53f63f
JeanJPNM <jeanjpnm1@gmail.com>
2022-04-03 12:03:18 -0300

    add ValueOwner class

---
+++ compiler/src/values/ValueOwner.ts
@@ -0,0 +1,64 @@
+import { IScope, IValue, IValueOwner } from "../types";
+import { StoreValue } from "./StoreValue";
+
+export interface IValueOwnerParams<T extends IValue> {
+  scope: IScope;
+  constant?: boolean;
+  value: T;
+  /** The identifier of the variable that `this` represents */
+  identifier?: string;
+  /** The name that the variable will have during transpilation to mlog */
+  name?: string;
+}
+
+export class ValueOwner<T extends IValue = IValue> implements IValueOwner<T> {
+  scope: IScope;
+  constant: boolean;
+  value: T;
+  /** The identifier of the variable that `this` represents */
+  identifier?: string;
+  /** The name that the variable will have during transpilation to mlog */
+  private _name?: string;
+
+  /**
+   * Creates a new `ValueOwner`, if `value` is already owned,
+   * `this` will be aliased to `value`, else `this` will own `value`
+   */
+  constructor({
+    scope,
+    constant = false,
+    value,
+    identifier,
+    name,
+  }: IValueOwnerParams<T>) {
+    this.scope = scope;
+    this.constant = constant;
+    this.value = value;
+    this.identifier = identifier;
+    this._name = name;
+    if (!value.owner) this.own(value);
+  }
+
+  get name() {
+    this._name ??= this.scope.makeTempName();
+    return this._name;
+  }
+
+  static temp(scope: IScope, name?: string) {
+    return new ValueOwner({
+      scope,
+      name,
+      value: new StoreValue(scope),
+    });
+  }
+
+  own(target: T): void {
+    this.value.owner = null;
+    target.owner = this;
+    this.value = target;
+  }
+
+  alias(owner: IValueOwner<T>): void {
+    this.value = owner.value;
+  }
+}
