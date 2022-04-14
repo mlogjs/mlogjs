0e173da4648967bbab8dccf4ff88805fab073808
JeanJPNM <jeanjpnm1@gmail.com>
2022-04-14 17:17:27 -0300

    document util functions

--- compiler/src/utils.ts
+++ compiler/src/utils.ts
@@ -6,12 +6,24 @@ import { es } from "./types";
 export const internalPrefix = "&";
 export const discardedName = `${internalPrefix}_`;
 
+/**
+ * Returns a string that has the format: [line]:[column]
+ */
 export function nodeName(node: es.Node) {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const { line, column } = node.loc!.start;
   return `${line}:${column}`;
 }
 
+/**
+ * Converts a camel case string into a dash case one.
+ *
+ * As an example `camelCase` becomes `camel-case`
+ * @example
+ * ```
+ * camelToDashCase("camelCase") // returns "camel-case"
+ * ```
+ */
 export function camelToDashCase(name: string) {
   return name.replace(/[A-Z]/g, str => `-${str.toLowerCase()}`);
 }
