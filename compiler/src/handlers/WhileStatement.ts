41570058082274c069758cace8d590c69f8a85c6
JeanJPNM <jeanjpnm1@gmail.com>
2022-04-30 16:36:29 -0300

    add the DoWhileStatement handler

--- compiler/src/handlers/WhileStatement.ts
+++ compiler/src/handlers/WhileStatement.ts
@@ -42,3 +42,43 @@ export const WhileStatement: THandler<null> = (
   );
   return [null, lines];
 };
+
+export const DoWhileStatement: THandler<null> = (
+  c,
+  scope,
+  node: es.DoWhileStatement
+) => {
+  const lines: IInstruction[] = [];
+  const [test, testLines] = c.handleConsume(scope, node.test);
+
+  const startLoopAddr = new LiteralValue(scope, null as never);
+  const endLoopAddr = new LiteralValue(scope, null as never);
+  const startLoopLine = new AddressResolver(startLoopAddr).bindContinue(scope);
+  const endLoopLine = new AddressResolver(endLoopAddr).bindBreak(scope);
+
+  if (test instanceof LiteralValue) {
+    if (test.data) {
+      lines.push(
+        startLoopLine,
+        ...c.handle(scope, node.body)[1],
+        new JumpInstruction(startLoopAddr, EJumpKind.Always),
+        endLoopLine
+      );
+    }
+    return [null, lines];
+  }
+
+  lines.push(
+    startLoopLine,
+    ...c.handle(scope, node.body)[1],
+    ...testLines,
+    new JumpInstruction(
+      startLoopAddr,
+      EJumpKind.Equal,
+      test,
+      new LiteralValue(scope, 1)
+    ),
+    endLoopLine
+  );
+  return [null, lines];
+};
