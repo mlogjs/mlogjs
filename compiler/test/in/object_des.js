e86500b0619de3ef4e0debf05e4c30dac2eba362
JeanJPNM <jeanjpnm1@gmail.com>
2022-05-14 17:25:41 -0300

    add nested destructuring to tests

--- compiler/test/in/object_des.js
+++ compiler/test/in/object_des.js
@@ -7,4 +7,20 @@ printFlush(getBuilding("message1"));
 
 // test with objetc macro
 ({ x, y, health } = { x: 10, y: 20, health: 200 });
+
+// test with nested object macro
+({
+  x,
+  a: {
+    y,
+    b: [health],
+  },
+} = {
+  x: 30,
+  a: {
+    y: 40,
+    b: [500],
+  },
+});
+
 print(x, y, health);
