7ce68d86a2704b1d4a310204e0efc2444448f39d
JeanJPNM <jeanjpnm1@gmail.com>
2022-05-14 17:23:12 -0300

    update destructuring tests

--- compiler/test/in/object_des.js
+++ compiler/test/in/object_des.js
@@ -1,6 +1,10 @@
-const { x, y, health } = getBuilding("cyclone1");
+let { x, y, health } = getBuilding("cyclone1");
 
 const [, , , { type: coreType }] = unitLocate("building", "core", true);
 
 print(x, y, health, coreType);
 printFlush(getBuilding("message1"));
+
+// test with objetc macro
+({ x, y, health } = { x: 10, y: 20, health: 200 });
+print(x, y, health);
