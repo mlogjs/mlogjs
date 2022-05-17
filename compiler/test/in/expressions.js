d042ebffbc191d52bb3522326cb9994798e35989
JeanJPNM <jeanjpnm1@gmail.com>
2022-05-17 17:01:31 -0300

    add tests for expressions

---
+++ compiler/test/in/expressions.js
@@ -0,0 +1,14 @@
+const pos = Math.rand(10);
+
+const content =
+  pos < 2 ? "low" : pos < 4 ? "medium" : pos < 6 ? "high" : "very high";
+
+print(content);
+
+const offset = pos - 5; // -5 to 5
+
+print(-offset);
+print(+offset);
+// should be evaluated at compile time to 10
+print(1 + 2 > 1.4 ? 10 : -5);
+print(1 + 1 != 2 ? "it's false" : "huh, true");
