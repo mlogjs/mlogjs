d68c3de3ed1cbe0d86c559c9587d8443c0539391
JeanJPNM <jeanjpnm1@gmail.com>
2022-05-21 19:45:10 -0300

    add more tests for built in commands

---
+++ compiler/test/in/misc_commands.js
@@ -0,0 +1,8 @@
+let seconds = 10;
+
+wait(seconds);
+
+let firstBlock = lookup("block", 0);
+let firstItem = lookup("item", 0);
+let firstLiquid = lookup("liquid", 0);
+let firstUnit = lookup("unit", 0);
