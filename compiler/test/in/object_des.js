b478b68c0c2d8cf1c90ad4d24decb1052c2b2dfc
JeanJPNM <jeanjpnm1@gmail.com>
2022-05-21 15:59:44 -0300

    fix object_des test

--- compiler/test/in/object_des.js
+++ compiler/test/in/object_des.js
@@ -28,6 +28,6 @@ printFlush(getBuilding("message1"));
 print(x, y, health, mem[0]);
 
 // tests with function return values
-const computed = enemy => unitLocate("building", "core", enemy);
+const computed = enemy => unitLocate("building", "core", enemy)[3];
 
 ({ x, y } = computed(true));
