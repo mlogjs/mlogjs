7397393dc286e69b53ca92df1a157fed9ef287ad
JeanJPNM <jeanjpnm1@gmail.com>
2022-06-13 15:14:52 -0300

    refactor editor/src/hooks/useSourceMapping.ts

--- editor/src/hooks/useSourceMapping.ts
+++ editor/src/hooks/useSourceMapping.ts
@@ -12,6 +12,8 @@ interface Params {
   sourcemaps: Sourcemaps | undefined;
 }
 
+const inlineClassName = "selection-highlighted";
+
 export function useSourceMapping({ editor, outEditor, sourcemaps }: Params) {
   useEffect(() => {
     if (!(editor && outEditor)) return;
@@ -22,29 +24,10 @@ export function useSourceMapping({ editor, outEditor, sourcemaps }: Params) {
     const inputListener = outEditor.onDidChangeCursorSelection(e => {
       outputDecorations = outEditor.deltaDecorations(outputDecorations, []);
       if (!sourcemaps) return;
-      const newDecorations: editor.IModelDeltaDecoration[] = [];
-      const { startLineNumber, endLineNumber } = e.selection;
 
-      for (let i = startLineNumber - 1; i <= endLineNumber - 1; i++) {
-        const source = sourcemaps[i];
-        if (!source) continue;
-        newDecorations.push({
-          options: {
-            inlineClassName: "selection-highlighted",
-          },
-          range: new monaco.Range(
-            source.start.line,
-            source.start.column + 1,
-            source.end.line,
-            source.end.column + 1
-          ),
-        });
-      }
-      inputDecorations = editor.deltaDecorations(
-        inputDecorations,
-        newDecorations
-      );
-      const firstSelection = sourcemaps[startLineNumber - 1];
+      const [firstSelection, decorations] = getInputDecorations(e, sourcemaps);
+      inputDecorations = editor.deltaDecorations(inputDecorations, decorations);
+
       if (
         firstSelection &&
         e.reason === monaco.editor.CursorChangeReason.Explicit
@@ -56,80 +39,18 @@ export function useSourceMapping({ editor, outEditor, sourcemaps }: Params) {
     const outputListener = editor.onDidChangeCursorSelection(e => {
       inputDecorations = editor.deltaDecorations(inputDecorations, []);
       if (!sourcemaps) return;
-      const { startLineNumber, startColumn, endLineNumber, endColumn } =
-        e.selection;
 
-      const selectionRange = new monaco.Range(
-        startLineNumber,
-        startColumn,
-        endLineNumber,
-        endColumn
-      );
-      const newDecorations: editor.IModelDeltaDecoration[] = [];
-
-      let decorationStart: number;
-      let outers: [number, SourceLocation][] = [];
-
-      for (let i = 0; i < sourcemaps.length; i++) {
-        const source = sourcemaps[i];
-        if (!source) continue;
-        const sourceRange = new monaco.Range(
-          source.start.line,
-          source.start.column + 1,
-          source.end.line,
-          source.end.column + 1
-        );
-        if (selectionRange.containsRange(sourceRange)) {
-          decorationStart ??= i + 1;
-          newDecorations.push({
-            options: {
-              inlineClassName: "selection-highlighted",
-              isWholeLine: true,
-            },
-            range: new monaco.Range(i + 1, 1, i + 1, 1),
-          });
-        } else if (sourceRange.containsRange(selectionRange)) {
-          if (outers.length === 0) {
-            outers.push([i, source]);
-          } else {
-            const [[, outer]] = outers;
-            const outerRange = new monaco.Range(
-              outer.start.line,
-              outer.start.column + 1,
-              outer.end.line,
-              outer.end.column + 1
-            );
-            if (outerRange.equalsRange(sourceRange)) {
-              outers.push([i, source]);
-            } else if (outerRange.containsRange(sourceRange)) {
-              outers = [[i, source]];
-            }
-          }
-        }
-      }
-
-      if (outers.length > 0 && newDecorations.length === 0) {
-        decorationStart = outers[0]?.[0];
-        for (const [i] of outers) {
-          newDecorations.push({
-            options: {
-              inlineClassName: "selection-highlighted",
-              isWholeLine: true,
-            },
-            range: new monaco.Range(i + 1, 1, i + 1, 1),
-          });
-        }
-      }
+      const [revealedLine, decorations] = getOutputDecorations(e, sourcemaps);
 
       outputDecorations = outEditor.deltaDecorations(
         outputDecorations,
-        newDecorations
+        decorations
       );
       if (
-        decorationStart &&
+        revealedLine &&
         e.reason === monaco.editor.CursorChangeReason.Explicit
       ) {
-        outEditor.revealLineInCenter(decorationStart);
+        outEditor.revealLineInCenter(revealedLine);
       }
     });
     return () => {
@@ -139,4 +60,108 @@ export function useSourceMapping({ editor, outEditor, sourcemaps }: Params) {
       outEditor.deltaDecorations(outputDecorations, []);
     };
   }, [editor, outEditor, sourcemaps]);
+}
+
+function getInputDecorations(
+  e: editor.ICursorSelectionChangedEvent,
+  sourcemaps: Sourcemaps
+): [SourceLocation | undefined, editor.IModelDeltaDecoration[]] {
+  const { startLineNumber, endLineNumber } = e.selection;
+  const decorations: editor.IModelDeltaDecoration[] = [];
+  const firstSelection = sourcemaps[startLineNumber - 1];
+
+  for (let i = startLineNumber - 1; i <= endLineNumber - 1; i++) {
+    const source = sourcemaps[i];
+    if (!source) continue;
+    decorations.push({
+      options: {
+        inlineClassName,
+      },
+      range: new monaco.Range(
+        source.start.line,
+        source.start.column + 1,
+        source.end.line,
+        source.end.column + 1
+      ),
+    });
+  }
+
+  return [firstSelection, decorations];
+}
+
+function getOutputDecorations(
+  e: editor.ICursorSelectionChangedEvent,
+  sourcemaps: Sourcemaps
+): [number, editor.IModelDeltaDecoration[]] {
+  const { startLineNumber, startColumn, endLineNumber, endColumn } =
+    e.selection;
+
+  const selectionRange = new monaco.Range(
+    startLineNumber,
+    startColumn,
+    endLineNumber,
+    endColumn
+  );
+
+  const decorations: editor.IModelDeltaDecoration[] = [];
+
+  let revealedLine: number | undefined;
+
+  // tracks source locations that contain
+  // the selected area, used if the selection
+  // doesn't contain any source locations
+  let outerSources: [number, SourceLocation][] = [];
+
+  for (let i = 0; i < sourcemaps.length; i++) {
+    const source = sourcemaps[i];
+    if (!source) continue;
+    const sourceRange = new monaco.Range(
+      source.start.line,
+      source.start.column + 1,
+      source.end.line,
+      source.end.column + 1
+    );
+
+    if (selectionRange.containsRange(sourceRange)) {
+      revealedLine ??= i + 1;
+      decorations.push({
+        options: {
+          inlineClassName,
+          isWholeLine: true,
+        },
+        range: new monaco.Range(i + 1, 1, i + 1, 1),
+      });
+    } else if (sourceRange.containsRange(selectionRange)) {
+      if (outerSources.length === 0) {
+        outerSources.push([i, source]);
+      } else {
+        const [[, outer]] = outerSources;
+        const outerRange = new monaco.Range(
+          outer.start.line,
+          outer.start.column + 1,
+          outer.end.line,
+          outer.end.column + 1
+        );
+        if (outerRange.equalsRange(sourceRange)) {
+          outerSources.push([i, source]);
+        } else if (outerRange.containsRange(sourceRange)) {
+          outerSources = [[i, source]];
+        }
+      }
+    }
+  }
+
+  if (outerSources.length > 0 && decorations.length === 0) {
+    revealedLine = outerSources[0]?.[0];
+    for (const [i] of outerSources) {
+      decorations.push({
+        options: {
+          inlineClassName,
+          isWholeLine: true,
+        },
+        range: new monaco.Range(i + 1, 1, i + 1, 1),
+      });
+    }
+  }
+  return [revealedLine, decorations];
 }
