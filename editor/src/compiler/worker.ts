5f43f792bbb500772359b1d5c80c714409b0d4ab
JeanJPNM <jeanjpnm1@gmail.com>
2022-06-13 08:56:39 -0300

    update compiling worker

--- editor/src/compiler/worker.ts
+++ editor/src/compiler/worker.ts
@@ -6,8 +6,21 @@ self.addEventListener("message", (e: MessageEvent<InputMessage>) => {
   try {
     const compiler = new Compiler(options);
 
+    const data = compiler.compile(input);
+
+    if (data[1]) {
+      // worker messages don't copy stuff from the prototype chain
+      // so this should make sure that the error is shared correctly
+      data[1] = {
+        loc: data[1].loc,
+        inner: data[1].inner,
+        name: data[1].name,
+        message: data[1].message,
+      };
+    }
+
     const outMessage: OutputMessage = {
-      data: compiler.compile(input),
+      data,
       id,
     };
 
