6260fdbde34106801f2fa9308dc85653fc4beaf6
JeanJPNM <jeanjpnm1@gmail.com>
2022-06-21 20:17:58 -0300

    return a senseable value from the `sensor` command

--- compiler/src/macros/commands/Sensor.ts
+++ compiler/src/macros/commands/Sensor.ts
@@ -1,8 +1,9 @@
 import { InstructionBase } from "../../instructions";
 import { MacroFunction } from "..";
-import { IScope } from "../../types";
+import { EMutability, IScope } from "../../types";
 import { SenseableValue, StoreValue } from "../../values";
 import { CompilerError } from "../../CompilerError";
+import { assign } from "../../utils";
 
 export class Sensor extends MacroFunction {
   constructor(scope: IScope) {
@@ -13,7 +14,9 @@ export class Sensor extends MacroFunction {
       if (!(target instanceof SenseableValue))
         throw new CompilerError("The sensor target must be a senseable value");
 
-      const temp = new StoreValue(scope);
+      const temp = assign(new SenseableValue(scope), {
+        mutability: EMutability.readonly,
+      });
       return [temp, [new InstructionBase("sensor", temp, target, property)]];
     });
   }
