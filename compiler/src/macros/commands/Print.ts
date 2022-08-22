e29bbca843c15d871a9a3b9a2bfb8355060d284e
JeanJPNM <jeanjpnm1@gmail.com>
2022-08-22 20:57:46 -0300

    make the Print macro work with template tags

--- compiler/src/macros/commands/Print.ts
+++ compiler/src/macros/commands/Print.ts
@@ -1,16 +1,36 @@
 import { InstructionBase } from "../../instructions";
 import { MacroFunction } from "..";
 import { IInstruction, IScope, IValue } from "../../types";
+import { LiteralValue } from "../../values";
+import { isTemplateObjectArray } from "../../utils";
 
 export class Print extends MacroFunction<null> {
   constructor(scope: IScope) {
     super(scope, (...values: IValue[]) => {
+      const [first] = values;
       const inst: IInstruction[] = [];
 
-      for (const value of values) {
-        inst.push(new InstructionBase("print", value));
+      if (!isTemplateObjectArray(first)) {
+        for (const value of values) {
+          inst.push(new InstructionBase("print", value));
+        }
+
+        return [null, inst];
       }
 
+      // `first` is likely a template strings array
+      // maybe this should be checked in another way?
+      const { length } = first.data;
+
+      for (let i = 1; i < values.length; i++) {
+        const [string] = first.get(scope, new LiteralValue(scope, i - 1));
+        inst.push(new InstructionBase("print", string));
+        inst.push(new InstructionBase("print", values[i]));
+      }
+
+      const [tail] = first.get(scope, new LiteralValue(scope, length.data - 1));
+
+      inst.push(new InstructionBase("print", tail));
       return [null, inst];
     });
   }
