2ae2f7312892d32ffc4a9ea5f75abd5e93fac1c2
JeanJPNM <jeanjpnm1@gmail.com>
2022-09-11 11:28:42 -0300

    move `asm` function to helpers.d.ts

--- compiler/lib/helpers.d.ts
+++ compiler/lib/helpers.d.ts
@@ -21,3 +21,17 @@ declare function getBuilding<T extends BasicBuilding = AnyBuilding>(
 ): T;
 
 declare function concat(...strings: string[]): string;
+
+/**
+ * Inlines raw mlog code. Variables and expressions can be placed inside.
+ *
+ * Although possible, using this function is heavily not recommended since
+ * you lose the benefits of getting type cheking and validation for some
+ * commands.
+ *
+ * ```js
+ *  let x = 10
+ *  asm`ucontrol build ${x} ${x * 2} @router 0 0`
+ * ```
+ */
+declare function asm(strings: TemplateStringsArray, ...values: unknown[]): void;
