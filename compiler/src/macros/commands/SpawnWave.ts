e9c242101e42d8ed27432a8d7d6bfdca97ad308c
JeanJPNM <jeanjpnm1@gmail.com>
2022-09-24 16:36:26 -0300

    fix the spawnWave command

--- compiler/src/macros/commands/SpawnWave.ts
+++ compiler/src/macros/commands/SpawnWave.ts
@@ -1,12 +1,30 @@
+import { CompilerError } from "../../CompilerError";
 import { InstructionBase } from "../../instructions";
 import { IScope } from "../../types";
+import { LiteralValue } from "../../values";
 import { MacroFunction } from "../Function";
 
 export class SpawnWave extends MacroFunction<null> {
   constructor(scope: IScope) {
-    super(scope, (natural, x, y) => [
-      null,
-      [new InstructionBase("spawnwave", x ?? "10", y ?? "10", natural)],
-    ]);
+    super(scope, (natural, x, y) => {
+      if (
+        !(natural instanceof LiteralValue) ||
+        (natural.data !== 1 && natural.data !== 0)
+      )
+        throw new CompilerError(
+          "The 'natural' argument must be a boolean literal"
+        );
+      return [
+        null,
+        [
+          new InstructionBase(
+            "spawnwave",
+            x ?? "10",
+            y ?? "10",
+            String(!!natural.data)
+          ),
+        ],
+      ];
+    });
   }
 }
