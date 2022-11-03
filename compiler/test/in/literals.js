28bcfc67cbee45bbdae4b4f99e7ef2b12fc1c780
JeanJPNM <jeanjpnm1@gmail.com>
2022-11-02 21:57:36 -0300

    add tests for the literal value short circuiting

--- compiler/test/in/literals.js
+++ compiler/test/in/literals.js
@@ -1,5 +1,19 @@
 const a = "string literal";
 const b = true;
 const c = 12;
+const d = false;
 
 print(a, b, c);
+
+const condition = Math.rand(c) > 10;
+
+print(
+  // b should be removed
+  b && condition,
+  // d should be removed
+  d || condition,
+  // evaluates to true
+  b || condition,
+  // evaluates to fales,
+  d && condition
+);
