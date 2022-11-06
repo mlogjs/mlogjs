5a9b10a30099ca916f687d66f676889dd5cafda1
JeanJPNM <jeanjpnm1@gmail.com>
2022-11-06 10:08:41 -0300

    fix the ObjectValue operators
    The operators are now defined with separate functions
    that are more specific to their arguments

--- compiler/src/values/ObjectValue.ts
+++ compiler/src/values/ObjectValue.ts
@@ -1,6 +1,10 @@
 import { CompilerError } from "../CompilerError";
 import { MacroFunction } from "../macros";
-import { operators } from "../operators";
+import {
+  leftRightOperators,
+  unaryOperators,
+  updateOperators,
+} from "../operators";
 import {
   EMutability,
   IScope,
@@ -41,7 +45,9 @@ export class ObjectValue extends VoidValue {
     return new ObjectValue(data);
   }
 
-  typeof(): TValueInstructions {
+  typeof(scope: IScope, out?: TEOutput): TValueInstructions {
+    const { $typeof } = this.data;
+    if ($typeof) return $typeof.call(scope, [], out);
     return [new LiteralValue("object"), []];
   }
 
@@ -70,19 +76,41 @@ export class ObjectValue extends VoidValue {
   ): TValueInstructions<IValue | null> {
     const { $call } = this.data;
     if (!$call) return super.call(scope, args, out);
-    return $call.call(scope, args);
+    return $call.call(scope, args, out);
   }
 }
 
-for (const op of operators) {
+for (const op of leftRightOperators) {
   ObjectValue.prototype[op] = function (
     this: ObjectValue,
-    ...args: [IScope, ...never[]]
+    scope: IScope,
+    value: IValue,
+    out?: TEOutput
   ) {
     const $ = this.data[`$${op}`];
-    // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-unsafe-return
-    if (!$) return (VoidValue.prototype[op] as Function).apply(this, args);
-    const [scope, ...fnArgs] = args;
-    return $.call(scope, fnArgs);
-  } as never;
+    if (!$) return VoidValue.prototype[op].apply(this, [scope, value, out]);
+    return $.call(scope, [value], out);
+  };
+}
+
+for (const op of unaryOperators) {
+  if (op === "typeof") continue;
+  ObjectValue.prototype[op] = function (scope: IScope, out?: TEOutput) {
+    const $ = this.data[`$${op}`];
+    if (!$) return VoidValue.prototype[op].apply(this, [scope, out]);
+    return $.call(scope, [], out);
+  };
+}
+
+for (const op of updateOperators) {
+  ObjectValue.prototype[op] = function (
+    this: ObjectValue,
+    scope: IScope,
+    prefix: boolean,
+    out?: TEOutput
+  ) {
+    const $ = this.data[`$${op}`];
+    if (!$) return VoidValue.prototype[op].apply(this, [scope, prefix, out]);
+    return $.call(scope, [new LiteralValue(+prefix)], out);
+  };
 }
