b75902b40594626470a64865d99d054c0d946bec
JeanJPNM <jeanjpnm1@gmail.com>
2022-11-07 18:59:38 -0300

    move `hideRedundantJumps` into the utils folder

--- compiler/src/utils/instruction.ts
+++ compiler/src/utils/instruction.ts
@@ -1,4 +1,5 @@
-import { es, IValue, TValueInstructions } from "../types";
+import { AddressResolver, JumpInstruction } from "../instructions";
+import { es, IInstruction, IValue, TValueInstructions } from "../types";
 
 export function appendSourceLocations<T extends IValue | null>(
   valueInst: TValueInstructions<T>,
@@ -17,4 +18,33 @@ export function withAlwaysRuns<T extends IValue | null>(
 ) {
   valueInst[1].forEach(inst => (inst.alwaysRuns = value));
   return valueInst;
+}
+
+/** Removes jump instructions that point right to the next line */
+export function hideRedundantJumps(inst: IInstruction[]) {
+  for (let i = 0; i < inst.length; i++) {
+    const instruction = inst[i];
+    if (!(instruction instanceof JumpInstruction)) continue;
+
+    const { address } = instruction;
+
+    let searchIndex = i + 1;
+    let current = inst[searchIndex];
+
+    while (current.hidden) {
+      if (
+        current instanceof AddressResolver &&
+        current.bonds.includes(address)
+      ) {
+        // only gets assigned if `current` is an adress resolver
+        // that resolves the jump adress
+        // and that is also right next to the jump,
+        // without any actual instructions in between
+        instruction.hidden = true;
+        break;
+      }
+      searchIndex++;
+      current = inst[searchIndex];
+    }
+  }
 }
