0c07b225e786e922b9258f4062e1762930168a6d
JeanJPNM <jeanjpnm1@gmail.com>
2022-12-10 13:36:49 -0300

    write jsdoc comments for the dynamic array members

--- compiler/lib/macros.d.ts
+++ compiler/lib/macros.d.ts
@@ -23,34 +23,100 @@ declare class Memory<S extends MemoryCapacity = 64> {
  */
 declare class MutableArray<T> {
   /**
-   * Creates a dynamic acess array, it can contain any type assignable to a store.
+   * Creates an array with fixed size that supports dynamic index accesses.
+   *
+   * The array can contain any value assignable to a store.
    */
   constructor(init: readonly T[]);
-
-  /**
-   * Creates a dynamic acess array, it can contain any type assignable to a store.
-   */
   constructor(length: number);
 
-  /** Checked index access. */
   [index: number]: T;
 
   /** Fills the array with a given value */
   fill(value: T): void;
+
+  /**
+   * Returns the item at the given index. Negative indexes are relative to `this.size`.
+   *
+   * ```js
+   * print(array.at(-1)); // equal to array[array.size - 1]
+   * ```
+   */
   at(index: number): T;
 
-  /** The number of items defined in this array */
+  /** The number of items preallocated for this array */
   readonly size: number;
 
   [Symbol.iterator](): IterableIterator<T>;
 }
 
 declare class DynamicArray<T> extends MutableArray<T> {
-  /** Pops the last item of the array */
+  /**
+   * Creates an array with a predefined capacity that supports dynamic index accesses.
+   *
+   * The array can contain any value assignable to a store.
+   */
+  constructor(init: readonly T[]);
+  constructor(length: number);
+
+  /**
+   * Sets the last item of the array to `null` and decreases its length by 1.
+   *
+   * This method only generates the instructions for getting the value when needed.
+   *
+   * You can use `unchecked` to skip the bound check.
+   *
+   * ```js
+   * array.pop(); // doesn't generate getter instructions
+   * print(array.pop()); // generates getter instructions
+   * unchecked(array.pop()); // doesn't check the array length
+   * ```
+   */
   pop(): T;
+  /**
+   * Adds the given item to the end of the array.
+   *
+   * If the array is full, the proccess is skipped.
+   *
+   * You can use `unchecked` to skip the length check.
+   *
+   * ```js
+   * // only do this if you know the array isn't full
+   * unchecked(array.push(Items.copper));
+   *
+   * array.push(Items.beryllium);
+   * ```
+   */
   push(item: T): void;
+
+  /**
+   * Removes the item at the given index.
+   *
+   * This method only generates the instructions for getting the value when needed.
+   *
+   * You can use `unchecked` to skip the bound check.
+   *
+   * ```js
+   *
+   * let a = array.removeAt(i); // generates getter instructions
+   * array.removeAt(i); // doesn't generate getter instructions
+   *
+   * // only do this if you know the index is valid
+   * unchecked(array.removeAt(i));
+   * ```
+   */
   removeAt(index: number): T;
 
+  /**
+   * Returns the item at the given index. Negative indexes are relative to `this.length`.
+   *
+   * ```js
+   * print(array.at(-1)); // equal to array[array.length - 1]
+   * ```
+   */
+  at(index: number): T;
+
+  /** The current length of the array, it can't be directly modified by the user. */
   get length(): number;
 }
 
