2cc2e0291d22c8884eda981c1886a89733eb5de4
JeanJPNM <jeanjpnm1@gmail.com>
2022-12-29 10:33:52 -0300

    remove the jump in if statements when possible
    This will make the handler check if the body of the if statement
    ends with something that already exits the if statement in some way

--- compiler/src/handlers/IfStatement.ts
+++ compiler/src/handlers/IfStatement.ts
@@ -1,6 +1,6 @@
 import { JumpInstruction, AddressResolver } from "../instructions";
 import { EJumpKind } from "../instructions";
-import { THandler, es, IInstruction } from "../types";
+import { THandler, es, IInstruction, EInstIntent } from "../types";
 import { pipeInsts, withAlwaysRuns } from "../utils";
 import { LiteralValue } from "../values";
 
@@ -15,22 +15,43 @@ export const IfStatement: THandler<null> = (c, scope, node: es.IfStatement) => {
   }
 
   const endIfAddr = new LiteralValue(null);
+  const consequent = withAlwaysRuns(c.handle(scope, node.consequent), false);
 
   inst.push(
     new JumpInstruction(endIfAddr, EJumpKind.Equal, test, new LiteralValue(0)),
-    ...withAlwaysRuns(c.handle(scope, node.consequent), false)[1]
+    ...consequent[1]
   );
 
   if (!node.alternate) {
     inst.push(new AddressResolver(endIfAddr));
   } else {
     const endElseAddr = new LiteralValue(null);
+    const needsJump = !endsWithControlFlow(consequent[1]);
+
+    if (needsJump)
+      inst.push(new JumpInstruction(endElseAddr, EJumpKind.Always));
+
     inst.push(
-      new JumpInstruction(endElseAddr, EJumpKind.Always),
       new AddressResolver(endIfAddr),
-      ...withAlwaysRuns(c.handle(scope, node.alternate), false)[1],
-      new AddressResolver(endElseAddr)
+      ...withAlwaysRuns(c.handle(scope, node.alternate), false)[1]
     );
+
+    if (needsJump) inst.push(new AddressResolver(endElseAddr));
   }
   return [null, inst];
 };
+
+/**
+ * Returns `true` if the instruction array ends with an instruction
+ * that already changes the control flow of the script (like `jump x always`, `end` , `stop`, etc)
+ */
+function endsWithControlFlow(inst: IInstruction[]) {
+  for (let i = inst.length - 1; i >= 0; i--) {
+    const instruction = inst[i];
+    if (instruction instanceof AddressResolver) return false;
+    if (instruction.hidden) continue;
+
+    return instruction.intent !== EInstIntent.none;
+  }
+  return false;
+}
