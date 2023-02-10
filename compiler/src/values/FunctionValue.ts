2ea12820b51f4a3cfd31316c1b04f55bd766dd0f
JeanJPNM <jeanjpnm1@gmail.com>
2023-02-10 17:08:16 -0300

    implement function parameter destructuring

--- compiler/src/values/FunctionValue.ts
+++ compiler/src/values/FunctionValue.ts
@@ -29,22 +29,38 @@ import { LiteralValue } from "./LiteralValue";
 import { StoreValue } from "./StoreValue";
 import { VoidValue } from "./VoidValue";
 import { SenseableValue } from "./SenseableValue";
+import { LazyValue } from "./LazyValue";
+import { AssignmentValue } from "./AssignmentValue";
+import {
+  DestructuringValue,
+  TDestructuringMembers,
+} from "./DestructuringValue";
 
 export type TFunctionValueInitParams = (childScope: IScope) => {
   paramStores: StoreValue[];
   paramNames: string[];
 };
+
+type FunctionParam = es.Function["params"][number];
+type TParamValue = SenseableValue | AssignmentValue | DestructuringValue;
+
 export class FunctionValue extends VoidValue implements IFunctionValue {
   name: string;
   mutability = EMutability.constant;
   macro = true;
 
   scope: IScope;
-  private out?: TEOutput;
   private childScope!: IScope;
-  private params: es.Identifier[];
-  private paramValues: IValue[] = [];
-  private paramNames: string[] = [];
+  private params: FunctionParam[];
+  private paramValues: TParamValue[] = [];
+  private paramNames: Map<TParamValue, string> = new Map();
+  private destructuringKeyData: Map<
+    IValue,
+    { inst: IInstruction[]; value: TParamValue }
+  > = new Map();
+  private paramOuts: IValue[] = [];
+  private minimumArgumentCount = 0;
+  private maximumArgumentCount = 0;
   private inst!: IInstruction[];
   private addr!: LiteralValue<number | null>;
   private temp!: SenseableValue;
@@ -77,7 +93,6 @@ export class FunctionValue extends VoidValue implements IFunctionValue {
     this.body = body;
     this.c = c;
     this.params = params;
-    this.out = out;
     this.name = extractOutName(out) ?? scope.makeTempName();
   }
 
@@ -88,14 +103,19 @@ export class FunctionValue extends VoidValue implements IFunctionValue {
   private initScope() {
     this.childScope = this.scope.createFunction(this.name);
     this.childScope.function = this;
-    for (const id of this.params) {
-      const name = nodeName(id, !this.c.compactNames && id.name);
-      const value = new SenseableValue(name, EMutability.mutable);
-      this.childScope.set(id.name, value);
+
+    for (let i = 0; i < this.params.length; i++) {
+      const value = this.unwrapParameter(this.params[i]);
       this.paramValues.push(value);
-      this.paramNames.push(id.name);
+      this.paramOuts.push(
+        value instanceof AssignmentValue ? value.left : value
+      );
+
+      if (!(value instanceof AssignmentValue))
+        this.minimumArgumentCount = i + 1;
     }
     this.callSize = this.paramValues.length + 2;
+    this.maximumArgumentCount = this.params.length;
   }
   private ensureInit() {
     if (this.initialized) return;
@@ -159,7 +179,9 @@ export class FunctionValue extends VoidValue implements IFunctionValue {
       : SenseableValue.from(scope, undefined, EMutability.mutable);
 
     const inst: IInstruction[] = this.paramValues
-      .map((param, i) => param["="](scope, args[i])[1])
+      .map(
+        (param, i) => param["="](scope, args[i] ?? new LiteralValue(null))[1]
+      )
       .reduce((s, c) => s.concat(c), [])
       .concat(
         ...this.ret["="](scope, callAddressLiteral)[1],
@@ -209,15 +231,19 @@ export class FunctionValue extends VoidValue implements IFunctionValue {
 
     // make a copy of the function scope
     const fnScope = this.childScope.copy();
+
+    this.tryingInline = true;
+    let inst: IInstruction[] = [];
     // hard set variables within the function scope
-    this.paramNames.forEach((name, i) => {
-      fnScope.hardSet(name, args[i]);
-    });
+    for (let i = 0; i < this.paramValues.length; i++) {
+      inst.push(
+        ...this.hardSetParameter(fnScope, this.paramValues[i], args[i])
+      );
+    }
 
     this.inlineEnd = new LiteralValue(null);
 
-    this.tryingInline = true;
-    let inst = this.c.handle(fnScope, this.body)[1];
+    inst.push(...this.c.handle(fnScope, this.body)[1]);
     this.tryingInline = false;
 
     const returnIndex = inst.findIndex(
@@ -236,10 +262,21 @@ export class FunctionValue extends VoidValue implements IFunctionValue {
 
   call(scope: IScope, args: IValue[], out?: TEOutput): TValueInstructions {
     this.ensureInit();
-    if (args.length !== this.paramNames.length)
+    if (
+      args.length < this.minimumArgumentCount ||
+      args.length > this.maximumArgumentCount
+    ) {
+      const min = this.minimumArgumentCount;
+      const max = this.maximumArgumentCount;
+      if (min !== max)
+        throw new CompilerError(
+          `Cannot call: expected ${min}-${max} arguments but got: ${args.length}`
+        );
       throw new CompilerError(
-        `Cannot call: expected ${this.paramNames.length} arguments but got: ${args.length}`
+        `Cannot call: expected ${min} arguments but got: ${args.length}`
       );
+    }
+    validateParameters(args, this.paramValues);
     const inlineCall = this.inlineCall(scope, args, out);
     hideRedundantJumps(inlineCall[1]);
     const inlineSize = inlineCall[1].filter(i => !i.hidden).length;
@@ -264,7 +301,7 @@ export class FunctionValue extends VoidValue implements IFunctionValue {
   preCall(): readonly IValue[] | undefined {
     this.ensureInit();
     if (this.inline || this.tryingInline) return;
-    return this.paramValues;
+    return this.paramOuts;
   }
 
   debugString(): string {
@@ -273,6 +310,159 @@ export class FunctionValue extends VoidValue implements IFunctionValue {
 
   toString() {
     return '"[macro FunctionValue]"';
+  }
+
+  private unwrapParameter(param: FunctionParam): TParamValue {
+    switch (param.type) {
+      case "Identifier": {
+        const name = nodeName(param, !this.c.compactNames && param.name);
+        const value = new SenseableValue(name, EMutability.mutable);
+        this.childScope.set(param.name, value);
+        this.paramNames.set(value, param.name);
+
+        return value;
+      }
+      case "AssignmentPattern": {
+        const left = this.unwrapParameter(param.left as FunctionParam);
+
+        const [rightValue, rightInst] = this.c.handleEval(
+          this.childScope,
+          param.right,
+          left
+        );
+        const right = new LazyValue(scope => {
+          if (this.inline || this.tryingInline) {
+            return this.c.handleEval(scope, param.right, left);
+          }
+          return [rightValue, [...rightInst]];
+        });
+
+        return new AssignmentValue(left, right);
+      }
+      case "ObjectPattern": {
+        const members: TDestructuringMembers = new Map();
+        for (const prop of param.properties) {
+          if (prop.type === "RestElement")
+            throw new CompilerError("Rest parameters are not supported");
+
+          const { key: propKey, value: propValue } = prop;
+          const propInst: IInstruction[] = [];
+
+          const key =
+            propKey.type === "Identifier" && !prop.computed
+              ? new LiteralValue(propKey.name)
+              : pipeInsts(
+                  this.c.handleEval(this.childScope, propKey),
+                  propInst
+                );
+
+          const value = this.unwrapParameter(propValue as FunctionParam);
+          const hasDefault = value instanceof AssignmentValue;
+          this.destructuringKeyData.set(key, { inst: propInst, value });
+
+          members.set(key, {
+            value: hasDefault ? value.left : value,
+            handler: (get, propExists) => {
+              return this.c.handle(this.childScope, prop, () => {
+                const inst = [...propInst];
+                if (propExists() || !hasDefault) {
+                  const input = pipeInsts(get(), inst);
+                  // assigns the output to the target value
+                  const output = pipeInsts(
+                    value["="](this.childScope, input),
+                    inst
+                  );
+                  return [output, inst];
+                }
+                const result = pipeInsts(
+                  value["="](this.childScope, new LiteralValue(null)),
+                  inst
+                );
+                return [result, inst];
+              });
+            },
+          });
+        }
+        return new DestructuringValue(members);
+      }
+      case "ArrayPattern": {
+        const members: TDestructuringMembers = new Map();
+
+        for (let i = 0; i < param.elements.length; i++) {
+          const element = param.elements[i];
+          if (!element) continue;
+          const value = this.unwrapParameter(element as FunctionParam);
+          const hasDefault = value instanceof AssignmentValue;
+
+          if (!value)
+            throw new CompilerError(
+              "Destructuring element must resolve to a value",
+              element
+            );
+
+          const key = new LiteralValue(i);
+
+          this.destructuringKeyData.set(key, { inst: [], value });
+          members.set(key, {
+            value: hasDefault ? value.left : value,
+            handler: (get, propExists) => {
+              return this.c.handle(this.childScope, element, () => {
+                if (propExists() || !hasDefault) {
+                  const inst = get();
+                  // assigns the output to the target value
+                  pipeInsts(value["="](this.childScope, inst[0]), inst[1]);
+                  return inst;
+                }
+
+                return value["="](this.childScope, new LiteralValue(null));
+              });
+            },
+          });
+        }
+        return new DestructuringValue(members);
+      }
+      default:
+        throw new CompilerError(
+          `Unsupported function parameter type: ${param.type}`
+        );
+    }
+  }
+
+  private hardSetParameter(
+    scope: IScope,
+    param: TParamValue,
+    value: IValue = new LiteralValue(null)
+  ): IInstruction[] {
+    if (param instanceof SenseableValue) {
+      const name = this.paramNames.get(param) as string;
+      scope.hardSet(name, value);
+      return [];
+    }
+
+    if (param instanceof AssignmentValue) {
+      const [result, inst] = value["??"](scope, param.right, param.left);
+
+      inst.push(
+        ...this.hardSetParameter(scope, param.left as TParamValue, result)
+      );
+
+      return inst;
+    }
+
+    const inst: IInstruction[] = [];
+    for (const [key, { value: out }] of param.members) {
+      const data = this.destructuringKeyData.get(key);
+      if (!data) continue;
+      inst.push(...data.inst);
+      const hasDefault = data.value instanceof AssignmentValue;
+      const input =
+        value.hasProperty(scope, key) || !hasDefault
+          ? pipeInsts(value.get(scope, key, out), inst)
+          : new LiteralValue(null);
+      inst.push(...this.hardSetParameter(scope, data.value, input));
+    }
+
+    return inst;
   }
 }
 
@@ -295,4 +485,20 @@ function endsWithReturn(fun: FunctionValue, inst: IInstruction[]) {
     );
   }
   return false;
+}
+
+function validateParameters(args: IValue[], params: IValue[]) {
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    const param = params[i];
+    if (
+      arg instanceof StoreValue &&
+      param instanceof AssignmentValue &&
+      param.left.macro
+    ) {
+      throw new CompilerError(
+        "Cannot pass a store to a function parameter that has a macro as a default value."
+      );
+    }
+  }
 }
