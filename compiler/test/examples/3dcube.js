53f16b19229be966deabdda9b218bb8254cc0dd5
JeanJPNM <jeanjpnm1@gmail.com>
2023-02-12 10:27:44 -0300

    update 3dcube example

--- compiler/test/examples/3dcube.js
+++ compiler/test/examples/3dcube.js
@@ -34,109 +34,28 @@ draw.color(255, 255, 255);
 while (true) {
   draw.clear(0, 0, 0);
 
-  drawLine(
-    points[0][0],
-    points[0][1],
-    points[0][2], // 0
-    points[0][3],
-    points[0][4],
-    points[0][5]
-  );
-  drawLine(
-    points[1][0],
-    points[1][1],
-    points[1][2], // 1
-    points[1][3],
-    points[1][4],
-    points[1][5]
-  );
-  drawLine(
-    points[2][0],
-    points[2][1],
-    points[2][2], // 2
-    points[2][3],
-    points[2][4],
-    points[2][5]
-  );
-  drawLine(
-    points[3][0],
-    points[3][1],
-    points[3][2], // 3
-    points[3][3],
-    points[3][4],
-    points[3][5]
-  );
-  drawLine(
-    points[4][0],
-    points[4][1],
-    points[4][2], // 4
-    points[4][3],
-    points[4][4],
-    points[4][5]
-  );
-  drawLine(
-    points[5][0],
-    points[5][1],
-    points[5][2], // 5
-    points[5][3],
-    points[5][4],
-    points[5][5]
-  );
-  drawLine(
-    points[6][0],
-    points[6][1],
-    points[6][2], // 6
-    points[6][3],
-    points[6][4],
-    points[6][5]
-  );
-  drawLine(
-    points[7][0],
-    points[7][1],
-    points[7][2], // 7
-    points[7][3],
-    points[7][4],
-    points[7][5]
-  );
-  drawLine(
-    points[8][0],
-    points[8][1],
-    points[8][2], // 8
-    points[8][3],
-    points[8][4],
-    points[8][5]
-  );
-  drawLine(
-    points[9][0],
-    points[9][1],
-    points[9][2], // 9
-    points[9][3],
-    points[9][4],
-    points[9][5]
-  );
-  drawLine(
-    points[10][0],
-    points[10][1],
-    points[10][2], // 10
-    points[10][3],
-    points[10][4],
-    points[10][5]
-  );
-  drawLine(
-    points[11][0],
-    points[11][1],
-    points[11][2], // 11
-    points[11][3],
-    points[11][4],
-    points[11][5]
-  );
+  drawLine(points[0]);
+  drawLine(points[1]);
+  drawLine(points[2]);
+  drawLine(points[3]);
+  drawLine(points[4]);
+  drawLine(points[5]);
+  drawLine(points[6]);
+  drawLine(points[7]);
+  drawLine(points[8]);
+  drawLine(points[9]);
+  drawLine(points[10]);
+  drawLine(points[11]);
 
   drawFlush(display);
 
   rotate(2, 5, Math.rand(2));
 }
 
-function drawLine(p1x, p1y, p1z, p2x, p2y, p2z) {
+/**
+ * @param {number[]} positions
+ */
+function drawLine([p1x, p1y, p1z, p2x, p2y, p2z]) {
   const z1 = a31 * p1x + a32 * p1y + a33 * p1z;
   const z2 = a31 * p2x + a32 * p2y + a33 * p2z;
   const persp1 = padding / (z1 + 3);
