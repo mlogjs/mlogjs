e6182d58965ca90bc75e4189b86fb21b89cca983
JeanJPNM <jeanjpnm1@gmail.com>
2023-02-26 13:37:35 -0300

    add the sources of the svg icons

--- website/src/icons/ClippyIcon.vue
+++ website/src/icons/ClippyIcon.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- from: https://github.com/microsoft/vscode-icons/blob/main/icons/light/clippy.svg -->
   <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       fill-rule="evenodd"
