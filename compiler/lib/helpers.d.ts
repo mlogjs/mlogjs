b209343250d366330dc2da8ceb719fdbaf49734b
JeanJPNM <jeanjpnm1@gmail.com>
2023-07-09 15:25:05 -0300

    get autocompletions for getBuildings

--- compiler/lib/helpers.d.ts
+++ compiler/lib/helpers.d.ts
@@ -1,69 +1,72 @@
 /// <reference path="./kinds.d.ts" />
 
-/**
- * Allows getting unlisted processor variables or symbols.
- *
- * Using this method is discouraged because most values are
- * already accessible under the namespaces `Units`, `Blocks`,
- * `Items`, `Liquids`, `ControlKind` and `Vars`.
- *
- * @param name The identifier of the value
- */
-declare function getVar<T>(name: string): T;
+import { TLogicLinkNames } from "./util";
 
-/**
- * Gets a building linked to the processor by its name.
- *
- * @param name
- */
-declare function getBuilding<T extends BasicBuilding = AnyBuilding>(
-  name: string,
-): T;
+declare global {
+  /**
+   * Allows getting unlisted processor variables or symbols.
+   *
+   * Using this method is discouraged because most values are
+   * already accessible under the namespaces `Units`, `Blocks`,
+   * `Items`, `Liquids`, `ControlKind` and `Vars`.
+   *
+   * @param name The identifier of the value
+   */
+  function getVar<T>(name: string): T;
 
-/**
- * Returns a record of all the buildings linked to this processor.
- *
- * The use of this function is preferred if you are assigning
- * buildings to multiple variables at once.
- *
- * ```js
- * const { switch1: toggle, cell1 } = getBuildings();
- * ```
- */
-declare function getBuildings<T extends BasicBuilding = AnyBuilding>(): Record<
-  string,
-  T
->;
+  /**
+   * Gets a building linked to the processor by its name.
+   *
+   * @param name
+   */
 
-type ConcatLiteral = string | number | boolean | undefined;
+  function getBuilding<T extends BasicBuilding = AnyBuilding>(name: string): T;
 
-/**
- * Concatenates a list of constant literal values.
- *
- * ```js
- * const name = "example";
- *
- * // works with normal arguments
- * const before = concat("before_", name);
- *
- * // works with tagged template strings
- * const after = concat`after_${name}`;
- * ```
- */
-declare function concat(
-  ...args: ConcatLiteral[] | [TemplateStringsArray, ...ConcatLiteral[]]
-): string;
+  /**
+   * Returns a record of all the buildings linked to this processor.
+   *
+   * The use of this function is preferred if you are assigning
+   * buildings to multiple variables at once.
+   *
+   * ```js
+   * const { switch1: toggle, cell1 } = getBuildings();
+   * ```
+   */
+  function getBuildings<T extends BasicBuilding = AnyBuilding>(): Record<
+    TLogicLinkNames<keyof BuildingSymbolTable> | (string & {}),
+    T
+  >;
 
-/**
- * Inlines raw mlog code. Variables and expressions can be placed inside.
- *
- * Although possible, using this function is heavily not recommended since
- * you lose the benefits of getting type cheking and validation for some
- * commands.
- *
- * ```js
- *  let x = 10
- *  asm`ucontrol build ${x} ${x * 2} @router 0 0`
- * ```
- */
-declare function asm(strings: TemplateStringsArray, ...values: unknown[]): void;
+  type ConcatLiteral = string | number | boolean | undefined;
+
+  /**
+   * Concatenates a list of constant literal values.
+   *
+   * ```js
+   * const name = "example";
+   *
+   * // works with normal arguments
+   * const before = concat("before_", name);
+   *
+   * // works with tagged template strings
+   * const after = concat`after_${name}`;
+   * ```
+   */
+  function concat(
+    ...args: ConcatLiteral[] | [TemplateStringsArray, ...ConcatLiteral[]]
+  ): string;
+
+  /**
+   * Inlines raw mlog code. Variables and expressions can be placed inside.
+   *
+   * Although possible, using this function is heavily not recommended since
+   * you lose the benefits of getting type cheking and validation for some
+   * commands.
+   *
+   * ```js
+   *  let x = 10
+   *  asm`ucontrol build ${x} ${x * 2} @router 0 0`
+   * ```
+   */
+  function asm(strings: TemplateStringsArray, ...values: unknown[]): void;
+}
--- compiler/lib/util.d.ts
+++ compiler/lib/util.d.ts
@@ -1,4 +1,3 @@
-import { NumericLiteral } from "@babel/types";
 import { WithSymbols } from "./traits";
 
 export type TRadarFilter =
@@ -83,3 +82,29 @@ export type TSettablePropMap<T extends BasicBuilding | BasicUnit> =
   T extends BasicBuilding
     ? WithSymbols<TBuildingSettableProps>
     : WithSymbols<TUnitSettableProps>;
+
+export type TLogicLinkNames<T extends string> =
+  `${TLogicLinkName<T>}${TLogicLinkDigit}`;
+
+/**
+ * Converts a the name of a block into it's logic link variant.
+ *
+ * The transformation happens as follows:
+ * - Remove the "Large" at the end of the name.
+ * - If the name is all lowercase, return it.
+ * - If the name is a single uppercase letter followed by lowercase letters,
+ * return the name in lowercase form.
+ * - Else, remove one letter from the beggining and repeat the process.
+ */
+export type TLogicLinkName<T extends string> = T extends `${infer First}Large`
+  ? TLogicLinkName<First>
+  : T extends Lowercase<string>
+  ? T
+  : T extends `${infer FirstChar extends Uppercase<string>}${infer Rest extends
+      Lowercase<string>}`
+  ? `${Lowercase<FirstChar>}${Rest}`
+  : T extends `${string}${infer Rest}`
+  ? TLogicLinkName<Rest>
+  : never;
+
+export type TLogicLinkDigit = 1 | 2 | 3 | 4 | 6 | 7 | 8 | 9;
