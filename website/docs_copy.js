4f1446e4fd6adc9d472f0d40ac6568450cc721dd
Q.Z.Lin <qzlin01@163.com>
2023-07-18 20:24:17 +0800

    docs:copy solve cross-platform issue (#194)

---
+++ website/docs_copy.js
@@ -0,0 +1,7 @@
+const fs = require('node:fs');
+console.log('# mkdir -p ../docs && cp -r docs/.vitepress/dist/. ../docs')
+if (!fs.existsSync('../docs'))
+    fs.mkdir('../docs', err => { if (err) console.error(err) })
+fs.cp('docs/.vitepress/dist', '../docs', { recursive: true },
+    err => { if (err) console.error(err) }
+)
