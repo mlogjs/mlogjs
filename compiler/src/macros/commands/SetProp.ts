805386a68f8ec49e989c3ee7941086d46ec9f641
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2023-10-21 10:27:51 -0300

    implement the new setProp syntax

--- compiler/src/macros/commands/SetProp.ts
+++ compiler/src/macros/commands/SetProp.ts
@@ -1,15 +1,65 @@
 import { CompilerError } from "../../CompilerError";
 import { InstructionBase } from "../../instructions";
+import { IScope, IValue, TEOutput, TValueInstructions } from "../../types";
+import {
+  LiteralValue,
+  ObjectValue,
+  VoidValue,
+  formatSenseablePropName,
+} from "../../values";
 import { MacroFunction } from "../Function";
 
-export class SetProp extends MacroFunction<null> {
+export class SetProp extends MacroFunction {
   constructor() {
-    super((scope, out, property, target, value) => {
-      if (!property) throw new CompilerError("Missing argument: property");
+    super((scope, out, target) => {
       if (!target) throw new CompilerError("Missing argument: target");
-      if (!value) throw new CompilerError("Missing argument: value");
 
-      return [null, [new InstructionBase("setprop", property, target, value)]];
+      return [new Settable(target), []];
     });
+  }
+}
+
+class SettableEntry extends VoidValue {
+  macro = true;
+  constructor(
+    public target: IValue,
+    public prop: IValue,
+  ) {
+    super();
+  }
+
+  eval(scope: IScope, out?: TEOutput | undefined): TValueInstructions {
+    return this.target.get(scope, this.prop, out);
+  }
+
+  "="(scope: IScope, value: IValue): TValueInstructions {
+    let prop: string | IValue = this.prop;
+    if (prop instanceof LiteralValue && prop.isString())
+      prop = formatSenseablePropName(prop.data);
+    return [value, [new InstructionBase("setprop", prop, this.target, value)]];
+  }
+
+  debugString(): string {
+    return "SettableEntry";
+  }
+
+  toMlogString(): string {
+    return "[macro SettableEntry]";
+  }
+}
+
+class Settable extends ObjectValue {
+  constructor(public target: IValue) {
+    super({});
+  }
+
+  get(
+    scope: IScope,
+    key: IValue,
+    out?: TEOutput | undefined,
+  ): TValueInstructions {
+    const entry = new SettableEntry(this.target, key);
+    if (out) return entry.eval(scope, out);
+    return [entry, []];
   }
 }
