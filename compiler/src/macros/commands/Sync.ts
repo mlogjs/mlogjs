37feed9c56327413c79b6270197381ec0cac7429
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2023-10-23 17:17:57 -0300

    make sync only accept a store value

--- compiler/src/macros/commands/Sync.ts
+++ compiler/src/macros/commands/Sync.ts
@@ -1,10 +1,17 @@
+import { CompilerError } from "../../CompilerError";
 import { InstructionBase } from "../../instructions";
+import { StoreValue } from "../../values";
 import { MacroFunction } from "../Function";
 
 export class Sync extends MacroFunction<null> {
   constructor() {
-    super((scope, out, value) => {
-      return [null, [new InstructionBase("sync", value)]];
+    super((scope, out, variable) => {
+      if (!variable) throw new CompilerError("Missing argument: variable");
+      if (!(variable instanceof StoreValue))
+        throw new CompilerError(
+          `Expected a store value, received [${variable.debugString()}]`,
+        );
+      return [null, [new InstructionBase("sync", variable)]];
     });
   }
 }
