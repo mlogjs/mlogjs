17baac6b2f91c1986fe20024ab64d21c1ee709cc
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2025-01-12 21:40:12 -0300

    fix implementation of print

--- compiler/src/macros/commands/Print.ts
+++ compiler/src/macros/commands/Print.ts
@@ -7,7 +7,6 @@ export class Print extends MacroFunction {
   constructor() {
     super((c, cursor, loc, ...values) => {
       const first = c.getValue(values[0]);
-
       if (!isTemplateObjectArray(c, first)) {
         for (const value of values) {
           cursor.addInstruction(new NativePrintInstruction(value, loc));
@@ -17,17 +16,16 @@ export class Print extends MacroFunction {
 
       // `first` is likely a template strings array
       // maybe this should be checked in another way?
-      const { length } = first.data;
+      const length = c.getValue(first.data.length) as LiteralValue<number>;
 
       for (let i = 1; i < values.length; i++) {
         const id = first.data[i - 1];
         const string = c.getValue(id) as LiteralValue<string>;
-
+        console.log(string);
         if (string.data)
           cursor.addInstruction(new NativePrintInstruction(id, loc));
         cursor.addInstruction(new NativePrintInstruction(values[i], loc));
       }
-
       const tailId = first.data[length.data - 1];
 
       const tail = c.getValue(tailId) as LiteralValue<string>;
