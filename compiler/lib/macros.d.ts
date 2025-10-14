1be519889b7abb3a0b243b2595cc5d879f5e8da2
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2025-10-14 19:38:11 -0300

    doc comments for StringView

--- compiler/lib/macros.d.ts
+++ compiler/lib/macros.d.ts
@@ -150,6 +150,11 @@ declare class DynamicArray<T> extends MutableArray<T> {
  */
 declare function unchecked<T>(expression: T): T;
 
+/**
+ * A view of a string that allows indexed access to its characters.
+ *
+ * Each character is represented by its UTF-16 code unit.
+ */
 declare class StringView {
   /**
    * Creates a view of the given string, allowing you to access its characters
