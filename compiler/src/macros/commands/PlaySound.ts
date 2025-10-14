7cce51bd436b123e397f979169f4a5aa50891504
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2025-10-14 19:36:10 -0300

    fix playsound implementation

--- compiler/src/macros/commands/PlaySound.ts
+++ compiler/src/macros/commands/PlaySound.ts
@@ -1,4 +1,5 @@
 import { InstructionBase } from "../../instructions";
+import { IValue } from "../../types";
 import { ObjectValue } from "../../values";
 import { createOverloadNamespace } from "../util";
 
@@ -19,17 +20,26 @@ export class PlaySound extends ObjectValue {
       // wait 1
 
       handler(scope, overload, out, ...args) {
-        const params = ["0", "0", "0", "0", "0", "0"];
-        let sound = "0";
+        let sound: string | IValue = "0";
+        let volume: string | IValue = "0";
+        let pitch: string | IValue = "0";
+        let pan: string | IValue = "0";
+        let x: string | IValue = "0";
+        let y: string | IValue = "0";
+        let limit: string | IValue = "0";
+
         // let volume =
         switch (overload) {
-          case "positional":
-            params[0] = "true";
-
+          case "positional": {
+            // playsound true id volume pitch _pan x y limit
+            [sound, volume, pitch, x, y, limit] = args;
             break;
-          case "global":
-            params[0] = "false";
-          // playsound false sound_id volume pitch pan _x _y limit
+          }
+          case "global": {
+            // playsound false id volume pitch pan _x _y limit
+            [sound, volume, pitch, pan, limit] = args;
+            break;
+          }
         }
         return [
           null,
@@ -37,7 +47,13 @@ export class PlaySound extends ObjectValue {
             new InstructionBase(
               "playsound",
               String(overload === "positional"),
-              ...args,
+              sound,
+              volume,
+              pitch,
+              pan,
+              x,
+              y,
+              limit,
             ),
           ],
         ];
